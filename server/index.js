e796acddccd8d297f0916f3f0b13d7c3606e2517
Sai Alankrutha Pandra Pagada <alankrutha236@gmail.com>
2021-07-06 14:35:50 -0400

    app

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors =require('cors')
+const app = express();
+const path = require('path');
+app.use(cors());
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'./index.html'));
+})
+
+const port= process.env.PORT||4000
+
+app.listen(port,function(){
+    console.log(`server is running on ${port}`)
+})
