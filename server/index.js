bfe59ca77294654e8f906c592f588c2309327864
Sai Alankrutha Pandra Pagada <alankrutha236@gmail.com>
2021-07-06 16:44:12 -0400

    final app

--- server/index.js
+++ server/index.js
@@ -3,16 +3,11 @@ const cors =require('cors')
 const app = express();
 const path = require('path');
 app.use(cors());
-app.use(express.static("public"));
+ app.use(express.static("public"));
 app.get('/', function(req, res) {
-    res.sendFile( path.join(__dirname, 'public/index.html') );
+    res.sendFile( path.join(__dirname, '../index.html') );
 })
-app.get('/styles', function(req, res) {
-    res.sendFile( path.join(__dirname, 'public/styles.css') );
-})
-app.get('/js', function(req, res) {
-    res.sendFile( path.join(__dirname, 'public/server.js') )
-})
+
 const port= process.env.PORT||4000
 
 app.listen(port,function(){
